1d5bdf8c4fa511f7514e20eaa7752ce76e73baf7
Rodrigo Palermo <avilapalermo@gmail.com>
2020-09-22 17:38:48 -0300

    Add contributor

--- src/app.js
+++ src/app.js
@@ -7,9 +7,34 @@ const morgan = require("morgan");
 require('dotenv-safe').config();
 let { verifyJWT }= require("./controller/AuthController")
 
-//Import Routes
+// import Routes
 const routeLogin = require("./routes/login");
 const routeUser = require("./routes/user");
+
+// bcrypt tests
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+// auto-gen a salt and hash
+let hash1 = 0
+bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    hash1 = hash
+    console.log(hash1)
+});
+
+// to check a password:
+// Load hash from your password DB.
+bcrypt.compare(myPlaintextPassword, hash1, function(err, result) {
+  // result == true
+  console.log(result)
+});
+bcrypt.compare(someOtherPlaintextPassword, hash1, function(err, result) {
+  // result == false
+  console.log(result)
+});
 
 // log file usign morgan
 // create a write stream (append mode)
@@ -17,6 +42,7 @@ const accessLogStream = fs.createWriteStream(
   path.join(__dirname, "log\\access.log"),
   { flags: "a" }
 );
+
 
 // database connection - using mongoose
 mongoose.connect("mongodb://localhost:27017/app_mobcursos", {
