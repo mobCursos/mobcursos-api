b6d2ea13891b4f741eac9bdeb1e0a01842ae5972
leonardonunes1 <vgbxx157@gmail.com>
2020-10-05 15:35:43 -0300

    Crud-Cursos

--- src/controller/CourseController.js
+++ src/controller/CourseController.js
@@ -26,3 +26,64 @@ exports.enroll = (req, res) => {
     res.send('Student successfully enrolled.')
   }
 };
+exports.list = (req, res) => {
+  Course.find((err, courses) => {
+    if (err) {
+      res.status(500).send({ msg: err });
+      return console.error(err);
+    }
+    res.json(courses);
+  });
+};
+
+// get by _id
+exports.get_by_id = (req, res) => {
+  Course.findOne({ _id: req.params.id }, (err, course) => {
+    if (course) {
+      res.json(course);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+};
+
+// add
+// todo: admin privilege
+exports.add = (req, res) => {
+  const newCourse = new Course(req.body);
+  newCourse.save((err, course) => {
+    if (err) return console.error(err);
+    res.status(201).json(course);
+  });
+};
+
+//alter
+exports.alter = (req, res) => {
+  let id = req.params.id;
+  let courseAlter = req.body;
+  Course.findOneAndUpdate(
+    { _id: id },
+    courseAlter,
+    { new: true },
+    (err, courseActual) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(courseActual);
+    }
+  );
+};
+
+exports.remove = (req, res) => {
+  const id = req.params.id;
+  Course.findOneAndDelete({ _id: id }, (err, course) => {
+    // TODO: handle err
+    if (err) {
+      console.log(err);
+    } else if (course === null) {
+      res.sendStatus(404);
+    } else {
+      res.json(course);
+    }
+  });
+};
--- src/routes/course.js
+++ src/routes/course.js
@@ -3,9 +3,11 @@ let router = express.Router();
 const controller = require("../controller/CourseController");
 const { authRole } = require("../controller/AuthController");
 
-router.get("/inscrever", authRole(['admin','aluno']), controller.enroll);
 router.post("/criar", authRole(['admin','prof']), controller.add);
+router.get("/:listar", controller.list);
+router.get("/:id", controller.get_by_id);
 router.put("/:id", controller.alter);
 router.delete("/:id", controller.remove);
+
 
 module.exports = router;
